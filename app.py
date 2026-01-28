f85e1210c423a4957884edb3da1a0c6b85708da5
carsonp45 <carsonp3@nycstudents.net>
2026-01-28 10:17:07 -0500

    Day 3

---
+++ app.py
@@ -0,0 +1,17 @@
+import turtle
+from turtle import *
+t = Turtle()
+
+t.forward(200)
+
+def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+square(200)
+turtle.done()
