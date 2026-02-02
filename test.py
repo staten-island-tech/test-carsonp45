e65cce23fea8e154186c75e03e4eaeb109993073
carsonp45 <carsonp3@nycstudents.net>
2026-02-02 10:17:08 -0500

    day 4

--- test.py
+++ test.py
@@ -54,12 +54,64 @@ equal() """
 
 """ for i in range(3):
     print(i) """
-for i in range(60):
+""" for i in range(1):
     t.forward(110)
-    t.left(120)
+    t.left(90)
     t.forward(110)
-    t.left(120)
+    t.left(90)
     t.forward(110)
-    t.left(1)
-t.speed(0)
-turtle.done()
+    t.left(90)
+    t.forward(110)
+t.speed(1000)
+
+for i in range(59):
+    t.forward(110)
+    t.left(90)
+    t.forward(110)
+    t.left(90)
+    t.forward(110)
+    t.left(90)
+    t.forward(110)
+    t.left(5)
+t.speed(1000) """
+""" """ """ sidelength = 100
+rotate = 90"""
+
+""" def square(x,y):
+    for i in range(4):
+        t.forward(x)
+        t.left(y)
+square(100,90) 
+def doubleSquares(iRange):
+    length = 25
+    for i in range(iRange):
+        square(length, 90)
+        length = length * 2
+doubleSquares(5) """
+""" def square(x,y):
+    for i in range(4):
+        t.forward(x)
+        t.left(y)
+square(100,90) 
+def addSquares(iRange):
+    length = 25
+    for i in range(iRange):
+        square(length, 90)
+        length += 25
+addSquares(5)
+ """
+def Square(x,y):
+    for i in range(4):
+        t.forward(x)
+        t.left(y)
+Square(5,90) 
+def addSquares(iRange):
+    length = 5
+    for i in range(iRange):
+        Square(length,90)
+        length += 5
+        t.right(5)
+        t.speed(0)
+addSquares(60)
+
+turtle.done() 
